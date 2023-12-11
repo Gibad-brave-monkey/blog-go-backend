80ac61709d00dca97117b216888865cf4ce2ef26
Ilyas Gibadullin <ilyas.gibadullin.199819987@gmail.com>
2023-12-11 19:36:07 +0300

    feat: 🎸 Upgrage config
    Add fetchFlagOrEnv fn

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"flag"
 	"os"
 
 	"github.com/ilyakaznacheev/cleanenv"
@@ -13,25 +14,39 @@ type Config struct {
 	Port int    `yaml:"port" env-required:"true"`
 }
 
-// mustLoad - this fn must Load
+// mustLoad - this fn must Load config file(required)
 // If not load he'll PANIC
 func MustLoad() *Config {
 	if err := godotenv.Load(); err != nil {
 		panic("Error")
 	}
 
-	configPath := os.Getenv("CONFIG_PATH")
-	if configPath == "" {
-		panic("ConfigPath is empty")
-	}
-
 	// TODO: Check file is exists or not
-
 	var cfg Config
+
+	configPath := fetchFlagOrEnv()
 
 	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
 		panic("can't read config.yaml file" + err.Error())
 	}
 
 	return &cfg
+}
+
+func fetchFlagOrEnv() string {
+	var configPath string
+
+	flag.StringVar(&configPath, "config-path", "", "config-path")
+	flag.Parse()
+
+	if configPath != "" {
+		return configPath
+	}
+
+	configPath = os.Getenv("CONFIG_PATH")
+	if configPath != "" {
+		return configPath
+	}
+
+	panic("ConfigPath is empty")
 }
